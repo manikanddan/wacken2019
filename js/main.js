ef8f6de7d6dcaed376785a06746d802f89abd6b9
Manik <manikanddan.chandrasekaran@stud.h-da.de>
2019-04-15 21:10:36 +0200

    cc

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 
   // fullpage customization
   $('#fullpage').fullpage({
-    sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+    sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F', '#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F', '#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
     sectionSelector: '.vertical-scrolling',
     slideSelector: '.horizontal-scrolling',
     navigation: true,
